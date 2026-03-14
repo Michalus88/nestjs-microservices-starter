39d841f9bef1a493c3fdd943a8692ac448e8390c
Michal Molenda <michalus88@gmail.com>
2026-03-14 20:47:04 +0100

    feat: Refactor Payments module to utilize RMQ for notifications and update configuration schema

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-import { LoggerModule, NOTIFICATIONS_SERVICE } from '@app/common';
+import { LoggerModule, NOTIFICATIONS_SERVICE, RmqModule } from '@app/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -12,25 +11,12 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       isGlobal: true,
       validationSchema: Joi.object({
         GRPC_URL: Joi.string().required(),
-        NOTIFICATIONS_HOST: Joi.string().required(),
-        NOTIFICATIONS_PORT: Joi.number().required(),
+        RABBITMQ_URI: Joi.string().required(),
         STRIPE_SECRET_KEY: Joi.string().required(),
       }),
     }),
     LoggerModule,
-    ClientsModule.registerAsync([
-      {
-        name: NOTIFICATIONS_SERVICE,
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.TCP,
-          options: {
-            host: configService.get('NOTIFICATIONS_HOST'),
-            port: configService.get('NOTIFICATIONS_PORT'),
-          },
-        }),
-        inject: [ConfigService],
-      },
-    ]),
+    RmqModule.register(NOTIFICATIONS_SERVICE, 'notifications'),
   ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
