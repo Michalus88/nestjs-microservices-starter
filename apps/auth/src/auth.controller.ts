ceac5381a5eafe1e79784f15cbff86e93069091c
Michal Molenda <michalus88@gmail.com>
2026-03-14 19:06:25 +0100

    refactor: Update AuthController to use gRPC method for authentication and modify AuthModule to include GRPC_URL

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Res, UseGuards } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { GrpcMethod, Payload } from '@nestjs/microservices';
 import { Response } from 'express';
 import { CurrentUser, UserDocument } from '@app/common';
 import { AuthService } from './auth.service';
@@ -21,7 +21,7 @@ export class AuthController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @MessagePattern('authenticate')
+  @GrpcMethod('AuthService', 'Authenticate')
   async authenticate(@Payload() data: any) {
     return data.user;
   }
